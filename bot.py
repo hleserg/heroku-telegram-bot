bd1c7da3ee3562b3ca3fd750ba1217b2545e83ba
Sergey Khlebnikov <hleserg@gmail.com>
2021-01-02 00:09:23 +0300

    sfdfdf

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
-#import redis
-import os
-import telebot
-#import psycopg2
-from flask import Flask, request
-#import logging
-#import time
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import ssl
+import logging
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 TOKEN  = os.environ['TELEGRAM_TOKEN']
+PORT  = os.environ['PORT']
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
 #r = redis.from_url(os.environ.get("REDIS_URL"))
@@ -18,28 +20,43 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Hi!')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def help_command(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Help!')
 
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+def echo(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(update.message.text)
 
 
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://hleserg-bot.herokuapp.com/' + TOKEN)
-    return "!", 200
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("TOKEN", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_webhook(listen = '127.0.0.1', port = PORT, url_path="https://hleserg-bot.herokuapp.com/")
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    main()
